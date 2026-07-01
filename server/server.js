907c6d99be9a91e7c260e9db74e1ba58975bf54a
adeimah <adeeimah22@gmail.com>
2026-07-01 19:25:07 +0700

    branch new

--- server/server.js
+++ server/server.js
@@ -8,14 +8,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/api/data', (req, res) => {
-    const data = {
-        message: 'This is some sample data from the server.',
-        timestamp: new Date(),
-    };
-    res.json(data);
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
